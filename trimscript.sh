4612f9a93b7123dcf8325d86478ab9dcbcb3175d
Roger Barthelson <rogerab@email.arizona.edu>
2016-03-14 20:43:45 -0700

    Add output directory to trimmomatic
    Added directory, directed output to it.

--- trimscript.sh
+++ trimscript.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp $3 trimfile.txt
+mkdir trimout
 cuts=`cat trimfile.txt | grep -v \#`
 if [ $1 == PE ]; then
 for x in $2/*
@@ -9,13 +10,13 @@ do
         if [[ "$x" =~ .*1\.fq$ ]]; 
         then
         z=$(basename $x 1.fq)
-                java -jar -Xmx1024m /root/Trimmomatic-0.33/trimmomatic-0.33.jar PE "$2"/"$z"'1.fq' "$2"/"$z"'2.fq' 'trmPr_'"$z"'1.fq' 'trmS_'"$z"'1.fq' 'trmPr_'"$z"'2.fq' 'trmS_'"$z"'2.fq' $cuts
+                java -jar -Xmx1024m /root/Trimmomatic-0.33/trimmomatic-0.33.jar PE "$2"/"$z"'1.fq' "$2"/"$z"'2.fq' trimout/'trmPr_'"$z"'1.fq' trimout/'trmS_'"$z"'1.fq' trimout/'trmPr_'"$z"'2.fq' trimout/'trmS_'"$z"'2.fq' $cuts
         fi
         
         if [[ "$x" =~ .*1\.fastq$ ]]; 
         then
         z=$(basename $x 1.fastq)
-                java -jar -Xmx1024m /root/Trimmomatic-0.33/trimmomatic-0.33.jar PE "$2"/"$z"'1.fastq' "$2"/"$z"'2.fastq' 'trmPr_'"$z"'1.fastq' 'trmS_'"$z"'1.fastq' 'trmPr_'"$z"'2.fastq' 'trmS_'"$z"'2.fastq' $cuts
+                java -jar -Xmx1024m /root/Trimmomatic-0.33/trimmomatic-0.33.jar PE "$2"/"$z"'1.fastq' "$2"/"$z"'2.fastq' trimout/'trmPr_'"$z"'1.fastq' trimout/'trmS_'"$z"'1.fastq' trimout/'trmPr_'"$z"'2.fastq' trimout/'trmS_'"$z"'2.fastq' $cuts
         fi
 done
 fi
@@ -24,6 +25,6 @@ for x in $2/*
 do
 				y=$(basename $x)
 				z='trimS'$y
-				java -jar -Xmx1024m /root/Trimmomatic-0.33/trimmomatic-0.33.jar SE $x $z $cuts
+				java -jar -Xmx1024m /root/Trimmomatic-0.33/trimmomatic-0.33.jar SE $x trimout/$z $cuts
 done
 fi
