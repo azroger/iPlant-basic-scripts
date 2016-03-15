fa7d43d98378e20abc750c48c9e49ce1c2ba8873
Roger Barthelson <rogerab@email.arizona.edu>
2016-03-15 15:24:45 -0700

    correct an error in trimmer file editing
    The trimmer file is modified to make the path Docker compatible. The first step at editing it had an error. This should fix it.

--- trimscript.sh
+++ trimscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #cp $3 trimfile.txt
-cat $4 | sed 's/ILLUMINACLIP:/ILLUMINACLIP:\/staging\//' > trimfile.txt
+cat $3 | sed 's/ILLUMINACLIP:/ILLUMINACLIP:\/staging\//' > trimfile.txt
 mkdir trimout
 cuts=`cat trimfile.txt | grep -v \#`
 if [ $1 == PE ]; then
