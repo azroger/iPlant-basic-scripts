52a553870ebf4a3a90eb8679c31dbdc7d9d308e6
Roger Barthelson <rogerab@email.arizona.edu>
2016-03-22 21:55:26 -0700

    Update distribute_files_PE.sh

--- distribute_files_PE.sh
+++ distribute_files_PE.sh
@@ -1,44 +1,44 @@
 #!/bin/bash
 
-# mkdir distributed_files
-# cd distributed_files
-# mv ../$1 ./
+mkdir Distributed_files
+cd Distributed_files
 mkdir Dir1 Dir2 Dir3 Dir4
+cd ..
 i=0
 for x in $1/*;
 do
          i=`expr $i + 1`;
         if (( $i == 1 )); 
         then
-        mv $x Dir1/
+        mv $x Distributed_files/Dir1/       
         fi
         if (( $i == 2 )); 
         then
-        mv $x Dir1/
+        mv $x Distributed_files/Dir1/
         fi
         if (( $i == 3 )); 
         then
-        mv $x Dir2/
+        mv $x Distributed_files/Dir2/
         fi
         if (( $i == 4 )); 
         then
-        mv $x Dir2/       
+        mv $x Distributed_files/Dir2/       
         fi
         if (( $i == 5 )); 
         then
-        mv $x Dir3/
+        mv $x Distributed_files/Dir3/
         fi
         if (( $i == 6 )); 
         then
-        mv $x Dir3/
+        mv $x Distributed_files/Dir3/
         fi
         if (( $i == 7 )); 
         then
-        mv $x Dir4/        
+        mv $x Distributed_files/Dir4/        
         fi
         if (( $i == 8 )); 
         then
-        mv $x Dir4/  
+        mv $x Distributed_files/Dir4/  
         i=0;
         fi
 done
