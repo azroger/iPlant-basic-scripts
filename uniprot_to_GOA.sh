4fc2c47bf1d51fdb5b379adb51a607258a878180
Roger Barthelson <rogerab@email.arizona.edu>
2016-06-17 16:36:21 -0700

    Create uniprot_to_GOA.sh
    This is the wrapper script that runs the app in the DE that adds GO annotation to the results of tab-delimited blastp maps to the results of Blastp_a_subset_of_Uniprot.  The app name is Add_GO_to_Blastp-uniprot_output.

---
+++ uniprot_to_GOA.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export HOME=/var/lib/condor
+
+#script to transfer GOA info from gene_association.goa_uniprot file for the agbase uniprot entries -- maintained in the Data Store for public access
+
+/usr/local2/icommands3.3/iget /iplant/home/shared/iplantcollaborative/protein_blast_dbs/go_info/gene_association.goa_uniprot
+
+
+blastfile=$1
+
+sed 's/_/\t/' < $blastfile | cut -f2 | uniq > blastids.txt
+
+mkdir "splitgoa"
+
+perl splitB.pl gene_association.goa_uniprot "splitgoa"
+
+for x in splitgoa/temp*
+do
+perl -e ' $col1=0; $col2=1; ($f1,$f2)=@ARGV; open(F2,$f2); while (<F2>) { s/\r?\n//; @F=split /\t/, $_; $line2{$F[$col2]} .= "$_\n" }; $count2 = $.; open(F1,$f1); while (<F1>) { s/\r?\n//; @F=split /\t/, $_; $x = $line2{$F[$col1]}; if ($x) { $num_changes = ($x =~ s/^/$_\t/gm); print $x; $merged += $num_changes } } warn "\nJoining $f1 column $col1 with $f2 column $col2\n$f1: $. lines\n$f2: $count2 lines\nMerged file: $merged lines\n"; ' blastids.txt $x >> goa_entries.txt 
+done
+
+echo "I think I'm done!"
